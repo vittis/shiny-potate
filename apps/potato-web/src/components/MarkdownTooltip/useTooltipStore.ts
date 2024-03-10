04b35e2691f013be48c83ae738564dcd3a10eff0
vittis <vitbolb1@gmail.com>
2024-03-10 01:00:17 -0300

    add tooltip settings and allow click

--- apps/potato-web/src/components/MarkdownTooltip/useTooltipStore.ts
+++ apps/potato-web/src/components/MarkdownTooltip/useTooltipStore.ts
@@ -2,14 +2,23 @@ import { create } from "zustand";
 
 interface TooltipState {
 	tooltipMode: "hover" | "click";
+	hoverDelay: number;
 	setTooltipMode: (tooltipMode: "hover" | "click") => void;
+	setHoverDelay: (hoverDelay: number) => void;
 }
 
 const useTooltipStore = create<TooltipState>()(set => ({
 	tooltipMode: (localStorage.getItem("tooltipMode") as "hover" | "click") ?? "hover",
+	hoverDelay: localStorage.getItem("hoverDelay")
+		? parseInt(localStorage.getItem("hoverDelay")!)
+		: 400,
 	setTooltipMode: tooltipMode => {
 		set({ tooltipMode });
 		localStorage.setItem("tooltipMode", tooltipMode);
+	},
+	setHoverDelay: hoverDelay => {
+		set({ hoverDelay });
+		localStorage.setItem("hoverDelay", hoverDelay.toString());
 	},
 }));
 
